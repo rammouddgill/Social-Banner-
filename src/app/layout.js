bf17da860293fe7b3315c8359e4ac6fd4a34e871
rammouddgill <rammouddgill@gmail.com>
2025-10-16 21:08:58 +0530

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -16,7 +16,7 @@ export default function RootLayout({ children }) {
       <html lang="en">
         <head>
           <meta name="viewport" content="width=device-width, initial-scale=1" />
-          <meta name="google-adsense-account" content="ca-pub-9830530451105892"/>
+    <meta name="google-adsense-account" content="ca-pub-7313920774464271"/>
         </head>
         <body>{children}</body>
       </html>
